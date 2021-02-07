e0be4c3abec7e2ed9ace23a503d1930378615c79
iladrien-hub <sliptchenko71@gmail.com>
2021-02-07 13:13:23 +0200

    timeout moved to settings.json

--- settings.py
+++ settings.py
@@ -3,7 +3,5 @@ LOGGING_FORMAT = "\033[2m%(asctime)s [%(levelname)s] [%(threadName)s/%(funcName)
 HEADERS = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
                          "Chrome/88.0.4324.104 Safari/537.36"}
 
-LISTENING_TIMEOUT = 60
-
 CACHE_DIR = ".cache/"
 SETTINGS = "settings.json"
